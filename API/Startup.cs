ac4ea7ce1a9dc7a22379478cf265cea95ace9365
Andy Parinas <andy@budgetpetproducts.com.au>
2021-03-28 10:10:24 +1000

    Implemented CQRS for Acitivity

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -46,6 +49,10 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(_config.GetSection("AllowedCors")["Host"]);
                 });
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
