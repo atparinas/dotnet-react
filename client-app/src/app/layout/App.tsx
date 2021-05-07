c9a6f675d5ce8b645763d4d4079f9d57d3a7b1a3
Andy Parinas <parinasadolfo@gmail.com>
2021-05-07 20:43:14 +1000

    Login Form

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,7 @@ import TestError from "../../features/errors/TestError";
 import {ToastContainer} from "react-toastify";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
+import LoginForm from "../../features/users/LoginForm";
 
 function App() {
 
@@ -31,6 +32,7 @@ function App() {
                                        component={ActivityForm} />
                                 <Route path='/errors' component={TestError} />
                                 <Route path='/server-error' component={ServerError} />
+                                <Route path='/login' component={LoginForm} />
                                 <Route component={NotFound} />
                             </Switch>
                         </Container>
